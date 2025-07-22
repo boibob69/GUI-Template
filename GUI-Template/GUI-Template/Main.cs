f4210a304a94921f56a97b6e32870d9176aad638
boibob <155044205+boibob69@users.noreply.github.com>
2025-07-21 19:44:26 -0500

    GUI-Template

---
+++ GUI-Template/GUI-Template/Main.cs
@@ -0,0 +1,31 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace TheCabin
+{
+    public class Main : MelonMod
+    {
+
+        // Creates the buttons & Puts the menu on screen
+        public override void OnInitializeMelon()
+        {
+            MelonEvents.OnGUI.Subscribe(DrawMenu, 100);
+            MelonEvents.OnGUI.Subscribe(buttons, 100);
+        }
+
+        // Creates the menu
+        private void DrawMenu()
+        {
+            GUI.Box(new Rect(10, 0, 300, 200), "YOUR MENU NAME HERE");
+        }
+
+        // Mods/Buttons
+        public static void buttons()
+        {
+            if (GUI.Button(new Rect(10, 0, 130, 15), "Quit Game"))
+            {
+                UnityEngine.Application.Quit();
+            }
+        }
+    }
+}
